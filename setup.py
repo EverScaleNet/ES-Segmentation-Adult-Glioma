875650df6af89e7469277be44317183bb2d0cf30
Marcel <mnem12321@gmail.com>
2024-07-01 17:10:05 +0200

    feat: Add data visualization functionality for EDA
    The commit adds data visualization functionality for exploratory data analysis (EDA). It includes the implementation of a data loading and transformation pipeline using MONAI, as well as a visualization function to display image and corresponding mask slices. Error handling for missing data directories and files is also included.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="src",
-    version="0.1.1",
+    version="0.1.2",
     description="Segmentation of adult gliomas post-treatment using MONAI and U-Net with PyTorch Lightning and Hydra",
     author="Marcel Musialek",
     author_email="mnem12321@gmail.com",
