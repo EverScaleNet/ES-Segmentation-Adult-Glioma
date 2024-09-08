0e62f702ebd259ea743f679aabd7e54536a31fe0
marcelele-0 <mnem12321@gmail.com>
2024-09-08 15:45:55 +0200

    refactor: Refactor UNet3D model implementation for glioma segmentation
    Refactor the UNet3D model implementation for glioma segmentation by updating the class structure and optimizing the code. This includes renaming variables, reorganizing the code, and improving the overall readability and maintainability of the model.
    
    The changes also include updating the model's forward pass to use a more efficient and modular approach, replacing the previous implementation with a more streamlined and intuitive one.
    
    These changes aim to enhance the performance and usability of the UNet3D model for glioma segmentation.

--- glioma_segmentation/models/UNet.py
+++ glioma_segmentation/models/UNet.py
@@ -1,35 +1,59 @@
+import torch
+import pytorch_lightning as pl
+from torch import nn
 import torch.nn.functional as F
-import torch.nn as nn
-from glioma_segmentation.models.components.UNet_components import DoubleConv3D, Down3D, Up3D, OutConv3D
+from .components.UNet_components import DoubleConv3D, Down3D, Up3D, OutConv3D
 
-class UNet3D(nn.Module):
-    def __init__(self, n_channels, n_classes, trilinear=True):
+class UNet3D(pl.LightningModule):
+    def __init__(self, in_channels, out_channels, init_features=32, lr=1e-4):
         super(UNet3D, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.trilinear = trilinear
+        self.lr = lr
+        
+        self.encoder1 = DoubleConv3D(in_channels, init_features)
+        self.pool1 = nn.MaxPool3d(2)
 
-        self.inc = DoubleConv3D(n_channels, 64)
-        self.down1 = Down3D(64, 128)
-        self.down2 = Down3D(128, 256)
-        self.down3 = Down3D(256, 512)
-        factor = 2 if trilinear else 1
-        self.down4 = Down3D(512, 1024 // factor)
-        self.up1 = Up3D(1024, 512 // factor, trilinear)
-        self.up2 = Up3D(512, 256 // factor, trilinear)
-        self.up3 = Up3D(256, 128 // factor, trilinear)
-        self.up4 = Up3D(128, 64, trilinear)
-        self.outc = OutConv3D(64, n_classes)
+        self.encoder2 = Down3D(init_features, init_features * 2)
+        self.encoder3 = Down3D(init_features * 2, init_features * 4)
+        self.encoder4 = Down3D(init_features * 4, init_features * 8)
+
+        self.bottleneck = DoubleConv3D(init_features * 8, init_features * 16)
+
+        self.upconv4 = Up3D(init_features * 16, init_features * 8)
+        self.upconv3 = Up3D(init_features * 8, init_features * 4)
+        self.upconv2 = Up3D(init_features * 4, init_features * 2)
+        self.upconv1 = Up3D(init_features * 2, init_features)
+
+        self.out_conv = OutConv3D(init_features, out_channels)
 
     def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.pool1(enc1))
+        enc3 = self.encoder3(enc2)
+        enc4 = self.encoder4(enc3)
+
+        bottleneck = self.bottleneck(enc4)
+
+        dec4 = self.upconv4(bottleneck, enc4)
+        dec3 = self.upconv3(dec4, enc3)
+        dec2 = self.upconv2(dec3, enc2)
+        dec1 = self.upconv1(dec2, enc1)
+
+        return self.out_conv(dec1)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        val_loss = F.cross_entropy(y_hat, y)
+        self.log('val_loss', val_loss)
+        return val_loss
